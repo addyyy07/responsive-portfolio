a0c045b6bfd95b4167c2b52b473b611a53aa1747
addyyy07 <ruvaifabhagvani786@gmail.com>
2024-10-22 11:45:36 +0530

    complete

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ruvaifa Bhagvani</span> I am a passionate and motivated web developer who is eager to contribute to building dynamic and user-friendly websites. I am excited to leverage my academic foundation in web development and hands-on experience in coding and design to create engaging online experiences. I am looking for an opportunity where I can apply my knowledge and skills for continuous improvement."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
